6d92fd4ceda729c365df80e3061c85867bcedc55
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-02-19 21:39:54 +0000

    dev

--- q2.py
+++ q2.py
@@ -9,10 +9,10 @@ def main():
     # path to rsa file
     path = "RSA-encrypted-3.txt"
     #public key [e=307, N=165073]
-    e = 307
-    N = 165073
-    # e will not be greater than 307
-    numList =
+    publicKey = [307, 165073]
+    decryptedString = enc.decryptFileGivenPublicKey(path, publicKey)
+    print(decryptedString)
+
     
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import encryption as enc
 
-e = 307
-N = 165073
+
 
